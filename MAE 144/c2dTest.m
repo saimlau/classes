3987b81d75ed906308cf8a282d12aa435afb72e2
saimlau <louis33338888@gmail.com>
2023-10-12 16:26:22 -0700

    HW1

---
+++ MAE 144/c2dTest.m
@@ -0,0 +1,10 @@
+clear,clc,close all
+syms z1 p1 omega
+Ds = RR_tf([-1],[0 -10],1);
+%Ds = RR_tf([],[-2 -1],3);
+Dz = SML_C2D_matched(Ds,1,0.0001i,true)
+
+Dzzz = c2d(zpk([-1],[0 -10],1),1,'matched')
+
+Ds2 = RR_tf([-z1],[0 -p1],1);
+Dz2 = SML_C2D_matched(Ds2,1,omega,true)
